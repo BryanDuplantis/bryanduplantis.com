3b00efb10dc1ccadd3badc5a50ab3c24ef5af583
Bryan Duplantis <duplantis@gmail.com>
2015-09-13 13:06:15 -0500

    updated content, deleted contact form

--- main.js
+++ main.js
@@ -11,39 +11,3 @@ $(document).ready(function(){
       }, 1000, 'swing');
   });
 });
-
-// Using $.ajax + Mandrill to send email from form
-$('#submitBtn').click(function() {
-          var contact_message = "Message: " + $('.contact_message').val() + " " + "Name: " + $('.contact_name').val() + " " + "Email: " + $('.contact_email').val();
-          console.log(contact_message);
-        $.ajax({
-          type: "POST",
-          url: "https://mandrillapp.com/api/1.0/messages/send.json",
-          data: {
-            "key": "0XTg7rA7pBbKSCKg4oIy9w",
-            "message": {
-                "html": contact_message,
-                "text": contact_message,
-                "from_email": "duplantis@gmail.com",
-                "from_name": $('.contact_name').val() + " - " + "Portfolio Site",
-                "to": [
-                    {
-                        "email": "duplantis@gmail",
-                        "name": "Bryan",
-                        "type": "to"
-                    }
-                ],
-                "headers": {
-                    "Reply-To": "duplantis@gmail"
-                }
-              }
-            }
-         });
-       });
-
-
-
-
-
-
-
